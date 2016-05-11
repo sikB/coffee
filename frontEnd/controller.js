bc450e99f1f47e513dca9c318173ba88d2570c34
sikB <sikhovski@icloud.com>
2016-05-11 17:10:18 -0400

    8

--- frontEnd/controller.js
+++ frontEnd/controller.js
@@ -1,6 +1,6 @@
 var angularCigars = angular.module('angularCigars', ['ngRoute', 'ngCookies']);
 angularCigars.controller('cigarController', function($scope, $http, $location, $cookies){
-	var apiUrl = 'http://bogdansportfolio.com:3000';
+	var apiUrl = 'http://bogdansportfolio.com:3000/';
 
 	// if($location.path() != 'register' || 'login'){
 	// 	$location.path('homePage');
